263a12be52afd90342111e3cabaa3dcf007db661
chensuyang <953043894@qq.com>
2025-01-09 13:53:22 +0800

    因为遇到了工程构建后的固件异常问题,所以重新恢复工程后使用git管理所有可能对固件有影响的文件.

---
+++ trashed_modified_files/autogen/sl_event_handler.c
@@ -0,0 +1,94 @@
+#include "sl_event_handler.h"
+
+#include "em_chip.h"
+#include "sl_device_init_nvic.h"
+#include "sl_device_init_dcdc.h"
+#include "sl_device_init_hfxo.h"
+#include "sl_device_init_lfrco.h"
+#include "sl_device_init_clocks.h"
+#include "sl_device_init_emu.h"
+#include "pa_conversions_efr32.h"
+#include "sl_rail_util_pti.h"
+#include "sl_rail_util_rf_path.h"
+#include "btl_interface.h"
+#include "sl_sleeptimer.h"
+#include "app_timer.h"
+#include "sl_bluetooth.h"
+#include "gpiointerrupt.h"
+#include "sl_iostream_rtt.h"
+#include "sl_mbedtls.h"
+#include "sl_mpu.h"
+#include "nvm3_default.h"
+#include "sl_spidrv_instances.h"
+#include "psa/crypto.h"
+#include "sli_protocol_crypto.h"
+#include "sl_iostream_init_instances.h"
+#include "sl_power_manager.h"
+#include "sl_rail_util_power_manager_init.h"
+
+void sl_platform_init(void)
+{
+  CHIP_Init();
+  sl_device_init_nvic();
+  sl_device_init_dcdc();
+  sl_device_init_hfxo();
+  sl_device_init_lfrco();
+  sl_device_init_clocks();
+  sl_device_init_emu();
+  bootloader_init();
+  nvm3_initDefault();
+  sl_power_manager_init();
+}
+
+void sl_driver_init(void)
+{
+  GPIOINT_Init();
+  //sl_spidrv_init_instances();
+}
+
+void sl_service_init(void)
+{
+  sl_sleeptimer_init();
+  sl_mbedtls_init();
+  sl_mpu_disable_execute_from_ram();
+  psa_crypto_init();
+  sli_aes_seed_mask();
+  sl_iostream_init_instances();
+}
+
+void sl_stack_init(void)
+{
+  sl_rail_util_pa_init();
+  sl_rail_util_pti_init();
+  sl_rail_util_rf_path_init();
+  sl_bt_init();
+  sl_rail_util_power_manager_init();
+}
+
+void sl_internal_app_init(void)
+{
+}
+
+void sl_platform_process_action(void)
+{
+}
+
+void sl_service_process_action(void)
+{
+  sli_app_timer_step();
+}
+
+void sl_stack_process_action(void)
+{
+  sl_bt_step();
+}
+
+void sl_internal_app_process_action(void)
+{
+}
+
+void sl_iostream_init_instances(void)
+{
+  sl_iostream_rtt_init();
+}
+
